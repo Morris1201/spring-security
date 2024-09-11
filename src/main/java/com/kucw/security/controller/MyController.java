54a4e2df1b93f52babc5596fa8d795573d1b9d60
morris <morris91re@gmail.com>
2024-09-11 17:36:53 +0800

    feat:Authentication 身份認證資訊

--- src/main/java/com/kucw/security/controller/MyController.java
+++ src/main/java/com/kucw/security/controller/MyController.java
@@ -1,14 +1,25 @@
 package com.kucw.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class MyController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String hello(Authentication authentication) {
+
+        // 取得使用者的帳號
+        String username = authentication.getName();
+
+        // 取得使用者權限
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        return "Hello " + username + "!";
     }
 
     @GetMapping("/welcome")
