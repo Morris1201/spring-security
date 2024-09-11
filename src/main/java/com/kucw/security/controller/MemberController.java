c6346725dfcdadbfdd754c8e0b0234e3ebed7977
morris <morris91re@gmail.com>
2024-09-11 16:26:17 +0800

    feat:PasswordEncoder 加鹽

--- src/main/java/com/kucw/security/controller/MemberController.java
+++ src/main/java/com/kucw/security/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.kucw.security.controller;
 import com.kucw.security.dao.MemberDao;
 import com.kucw.security.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private MemberDao memberDao;
 
     @PostMapping("/register")
     public Member register(@RequestBody Member member) {
+
+        // hash 原始密碼
+        String hashedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(hashedPassword);
 
         // 在資料庫中插入 Member 數據
         Integer memberId = memberDao.createMember(member);
--- src/main/java/com/kucw/security/security/MySecurityConfig.java
+++ src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -18,7 +19,7 @@ public class MySecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -33,7 +34,6 @@ public class MySecurityConfig {
                         .anyRequest().denyAll()
                 )
                 .build();
-        //測試
     }
 
 //    @Bean
